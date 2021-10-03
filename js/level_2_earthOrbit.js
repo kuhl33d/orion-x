1a2a77a107873a33df7ca85642c83f9366222175
Khaled Abdel-Nasser <44317515+kuhl33d@users.noreply.github.com>
2021-10-03 11:47:23 +0200

    Update level_2_earthOrbit.js

--- js/level_2_earthOrbit.js
+++ js/level_2_earthOrbit.js
@@ -6,14 +6,14 @@ function preload(){
 }
 function setup() {
     createCanvas(1240, 780)
-    earth = new body(100,createVector(0,0),createVector(0,0),274,earthImg)
+    earth = new body(100,createVector(0,0),createVector(0,0),100,earthImg)
     let r = random(earth.r, min(windowWidth/2,windowHeight/2))//min max
     let theta = random(TWO_PI)
     let rocketPos = createVector(r*cos(theta),r*sin(theta))//x , y
     let rocketVel = rocketPos.copy()
     rocketVel.rotate(HALF_PI)
     rocketVel.setMag( sqrt(earth.G*earth.mass/rocketPos.mag()) )
-    rocket = new body(random(5,45),rocketPos,rocketVel,0,rImg)
+    rocket = new body(100,rocketPos,rocketVel,0,rImg)
 }
 
 function draw(){
